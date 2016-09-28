3b130c52db57ecf846c618341faaf338baf5d34f
Conan Lee <liyijia@shinetechchina.com>
2016-09-28 13:43:52 +0800

    Prepare Domain Model

---
+++ Ch01/Tutorial/Domain/Entities/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Domain.Entities
+{
+    public class Student
+    {
+        // Every Model must has its Key attribute, it represents the Primary Key in Sql Server
+        [Key]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool Sex { get; set; }
+    }
+}
---
+++ Ch01/Tutorial/Domain/TutorialEfDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using Domain.Entities;
+
+
+namespace Domain
+{
+    public class TutorialEfDbContext:DbContext
+    {
+        // base("TutorialEfDbContext") is very important,because the string "TutorialEfDbContext" in it must be the same as connectionStrings name in the config file 
+        public TutorialEfDbContext():base("TutorialEfDbContext")
+        {
+            Configuration.ProxyCreationEnabled = false; //disable proxy
+            Configuration.LazyLoadingEnabled = false; //disable lazy load
+        }
+
+        public DbSet<Student> Student { get; set; }
+    }
+}
